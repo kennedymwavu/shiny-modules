a162868bf5010629f81570ffe47c770bdadaad08
KennedyMwavu <mwavukennedy@gmail.com>
2022-03-27 00:41:53 +0300

    add fn documentation; center columns

--- R/datatable_options.R
+++ R/datatable_options.R
@@ -1,3 +1,17 @@
+#' Custom `DT::datatable()` options.
+
+#' This function provides some nice defaults for rendering `DT::datatables()`
+#' which are also customizable if need arises.
+#'
+#' @param given_dataframe An object of class data.frame
+#' @param scrollY Visible table height
+#' @param scrollX Logical: Do you want to enable horizontal scrolling?
+#' @param scroller Logical: Should the scroller extension be used?
+#' @inheritParams DT::datatable
+#' @return An object of class `datatables` and `htmlwidgets`
+#' @examples
+#' iris |> datatable_options()
+
 datatable_options <- function(
     given_dataframe,
     extensions = "Scroller",
@@ -8,13 +22,20 @@ datatable_options <- function(
 ) {
   DT::datatable(
     given_dataframe,
+    rownames = FALSE,
     extensions = extensions,
     class = class,
     options = list(
       deferRender = TRUE,
       scrollY = scrollY,
       scrollX = scrollX,
-      scroller = scroller
+      scroller = scroller,
+      columnDefs = list(
+        list(
+          className = 'dt-center',
+          targets = "_all"
+        )
+      )
     )
   )
 }
